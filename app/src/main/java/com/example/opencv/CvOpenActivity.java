0ced04b989dfd7549e6be941ebc873831d229c43
Carlos Cruzado <carlos.cruzado@zoluxiones.com>
2019-05-13 17:03:42 -0500

    feat: test cvopen

---
+++ app/src/main/java/com/example/opencv/CvOpenActivity.java
@@ -0,0 +1,14 @@
+package com.example.opencv;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class CvOpenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cvopen_activity);
+    }
+}
